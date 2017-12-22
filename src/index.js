71e5f35cbd42e2f003e0bd8e265984b153e8de7f
ArtemZV <zakharchukartem@gmail.com>
2017-12-22 17:50:14 +0200

    css fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css'
 import MuiThemeProvider from '../node_modules/material-ui/styles/MuiThemeProvider';
 
 const Index = () => (
